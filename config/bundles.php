c4dcfb3a3a9565adfa6dd347e2a7feea881cbc82
marmorag <g.marmo@hotmail.fr>
2019-10-23 23:05:48 +0200

    add monolog bundle & bref integration

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
