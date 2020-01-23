0f13e380a7881eb58349ae3a38ddfcb386321c33
marmorag <g.marmo@hotmail.fr>
2020-01-23 22:17:38 +0100

    fix services

--- src/Service/ApiTokenGenerator.php
+++ src/Service/ApiTokenGenerator.php
@@ -4,7 +4,7 @@ namespace App\Service;
 
 class ApiTokenGenerator
 {
-    private $prefix = 'mygstatapi-token:';
+    private string $prefix = 'mygstatapi-token:';
 
     public function generate() : string
     {
